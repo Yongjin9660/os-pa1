532c923f087a8c57ab001c569a5613cd1dc8139e
Kimyongjin <dydwls0669@ajou.ac.kr>
2020-03-28 02:02:09 +0900

    initial commit

--- pa1.c
+++ pa1.c
@@ -17,6 +17,10 @@
 #include <stdlib.h>
 #include <string.h>
 #include <getopt.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <unistd.h>
+#include <signal.h>
 
 #include "types.h"
 #include "parser.h"
@@ -48,8 +52,6 @@ static void set_timeout(unsigned int timeout)
 }
 /*          ****** DO NOT MODIFY ANYTHING UP TO THIS LINE ******      */
 /*====================================================================*/
-
-
 /***********************************************************************
  * run_command()
  *
@@ -62,19 +64,97 @@ static void set_timeout(unsigned int timeout)
  *   Return 0 when user inputs "exit"
  *   Return <0 on error
  */
+
 static int run_command(int nr_tokens, char *tokens[])
 {
 	/* This function is all yours. Good luck! */
-
 	if (strncmp(tokens[0], "exit", strlen("exit")) == 0) {
 		return 0;
 	}
+	else if(strncmp(tokens[0], "for",strlen("for")) == 0){
+		int size = 0;
+		while(tokens[size] != NULL){
+			size++;
+		}
+		size++;
+		int forN = 0;
+		for(int i=0 ; i < size-1 ; i++){
+			if(strncmp(tokens[i],"for",strlen("for"))==0)
+				forN++;
+		}
+		int index = size - 2*forN;
+		char* newTokens[index];
+		int j=0;
+		for(int i = forN*2 ; i < size -1 ; i++){
+			newTokens[j] = tokens[i];
+			j++;
+		}
+		newTokens[index-1] = NULL;
+		int forLoop=1;
+		for(int i=1 ; i < forN*2; i+=2){
+			forLoop = forLoop * atoi(tokens[i]);
+		}
+		if( strncmp(tokens[forN*2],"cd", strlen("cd")) ==0){
+			for(int i=0 ; i < forLoop ; i++){
+				if(strcmp(newTokens[1], "~") == 0) chdir(getenv("HOME"));
+				else chdir(newTokens[1]);
+			}
+		}
+		else{
+			for(int i=0 ; i<forLoop ; i++){
+				pid_t pid;
+				int status;
+				pid = fork();
+				if(pid==0){
+					if(execvp(newTokens[0],newTokens)==-1) 
+						fprintf(stderr,"No such file or directory\n");
+					close(0);
+					exit(0);
+				}else
+					wait(&status);
+			}
+			if(execvp(tokens[0],tokens)==-1) 
+				fprintf(stderr,"No such file or directory\n");
+		}
+		return 1;
+	}
+	else if(strncmp(tokens[0] , "prompt", strlen("prompt")) == 0){
+		strcpy(__prompt,tokens[1]);
+		return 1;
+	}	
+	else if( strncmp(tokens[0] , "cd", strlen("cd")) == 0 ){
+		if(strcmp(tokens[1], "~") == 0) chdir(getenv("HOME"));
+		else chdir(tokens[1]);
+		return 1;
+	}
+	
+	else if(strncmp(tokens[0] , "timeout", strlen("timeout")) == 0){
+		if(tokens[1]==NULL){
+			fprintf(stderr,"Current time out is %d second\n",__timeout);
+		}
+		else{
+			set_timeout(atoi(tokens[1]));
+		}
+		return 1;
+	}
+	
+	pid_t pid;
+	int status=-1;
+	pid = fork();
 
-	/*
-	fork();
-	exec();
-	...
-	*/
+	if(pid==0){		//child process
+		close(0);
+		alarm(__timeout);
+		if(execvp(tokens[0],tokens)==-1){
+			fprintf(stderr,"No such file or directory\n");
+		}
+		exit(0);
+		}
+	else{	//parent process
+		wait(&status);
+		if(WIFSIGNALED(status)) 
+			fprintf(stderr,"%s is timed out\n", tokens[0]);
+	}
 
 	return 1;
 }
@@ -106,7 +186,6 @@ static int initialize(int argc, char * const argv[])
  */
 static void finalize(int argc, char * const argv[])
 {
-
 }
 
 
@@ -125,7 +204,6 @@ int main(int argc, char * const argv[])
 	char command[MAX_COMMAND_LEN] = { '\0' };
 	int ret = 0;
 	int opt;
-
 	while ((opt = getopt(argc, argv, "qm")) != -1) {
 		switch (opt) {
 		case 'q':
